fe82e4990a2b8578c81c518c65950d35605504db
TheRealPersonOng <131401125+therealpersong@users.noreply.github.com>
2025-02-16 17:38:55 +0000

    Update App.py
    Shopping cart program

--- App.py
+++ App.py
@@ -1,16 +1,25 @@
-import time
-try:
-    my_time = int(input("Enter the time in seconds: "))
-except:
-    print("Has to be a number")
+#shopping cart program
 
-for x in range(my_time,0,-1):
-    seconds = x % 60
-    minutes = int(x / 60) % 60
-    hours = int(x / 3600)
-    print(f"{hours:02}:{minutes:02}:{seconds:02}")
-    time.sleep(1)
+foods = []
+prices = []
+total = 0
 
+while True:
+    food = input("Enter a food to buy: (q to quit)")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of {food}: $"))
+        foods.append(food)
+        prices.append(price)
 
-print("TIME'S UP!")
+print("----- YOUR CART -----")
 
+for food in foods:
+    print(food, end=" ")
+
+for price in prices:
+    total += price
+
+print()
+print(f"Your total is: ${total}")
