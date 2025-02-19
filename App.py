0a42c38f18ef41d99b1d5872b12d9661a1143229
TheRealPersonOng <131401125+therealpersong@users.noreply.github.com>
2025-02-19 19:57:37 +0000

    Update App.py
    improved

--- App.py
+++ App.py
@@ -1,20 +1,34 @@
 import random
 
-low = 1
-high = 100
+lowest_num = 1
+highest_num = 100
+answer = random.randint(lowest_num, highest_num)
 guesses = 0
-number = random.randint(low, high)
+is_running = True
 
-while True:
-    guess = int(input(f"Enter a number between ({low} - {high}): "))
-    guesses += 1
+print("Python number guessing game")
+print(f"I'm thinking of a number between {lowest_num} and {highest_num}.")
 
-    if guess < number:
-        print(f"{guess} is too low")
-    elif guess > number:
-        print(f"{guess} is too high")
+while is_running:
+
+    guess = input("Enter your guess: ")
+    
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+        if guess < lowest_num or guess > highest_num:
+            print("That number is out of range")
+            print(f"please select a number between {lowest_num} and {highest_num}.")
+        elif guess < answer:
+            print("Too low! try again")
+
+        elif guess > answer:
+            print("too high try again")
+        else:
+            print(f"Correct! the answer was {answer}")
+            print(f"you got it in {guesses} guesses")
+            is_running = False
     else:
-        print(f"{guess} is correct!")
-        break
-
-print(f"This round took you {guesses} guesses")
+        print("Invalid guess")
+        print(f"please select a number between {lowest_num} and {highest_num}.")
