6560115e694edc722d9ff38e20328343efb0b01a
TheRealPersonOng <131401125+therealpersong@users.noreply.github.com>
2025-02-22 19:45:15 +0000

    Update App.py
    practicing functions for recap

--- App.py
+++ App.py
@@ -1,35 +1,7 @@
-import random
-import string
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
 
-chars = " " + string.punctuation + string.digits + string.ascii_letters
-chars = list(chars)
-key = chars.copy()
-
-random.shuffle(key)
-
-#print(f"chars: {chars}")
-#print(f"key: {key}")
-
-#ENCRYPT
-plain_text = input("Enter a message to encrypt: ")
-cipher_text = ""
-
-for letter in plain_text:
-    index = chars.index(letter)
-    cipher_text += key[index]
-
-print(f"Original message: {plain_text}")
-
-print(f"Encrypted message: {cipher_text}")
-
-#DECRYPT
-cipher_text = input("Enter a message to dencrypt: ")
-plain_text = ""
-
-for letter in cipher_text:
-    index = key.index(letter)
-    plain_text += chars[index]
-
-print(f"Encrypted message: {cipher_text}")
-print(f"Original message: {plain_text}")
-
+full_name = create_name("Bro", "Code")
+print(full_name)
