12c7e138674e446f95cb0b6de8431c04a9afe069
TheRealPersonOng <131401125+therealpersong@users.noreply.github.com>
2025-02-24 09:28:58 +0000

    Update App.py
    args and kwargs

--- App.py
+++ App.py
@@ -1,9 +1,52 @@
-import time
+# ----- *ARGS Example 1 -----
 
-def count(end,start=0):
-    for x in range(start, end+1):
-        print(x)
-        time.sleep(1)
-    print("Done")
+def add(*nums):
+   total = 0
+   for num in nums:
+       total += num
+   return total
 
-count(10)
+print(add(1, 2, 3, 4))
+
+# ----- *ARGS Example 2 -----
+
+def display_name(*args):
+   print(f"Hello", end=" ")
+   for arg in args:
+       print(arg, end=" ")
+
+display_name("Dr.", "Spongebob", "Harold", "Squarepants", "III")
+
+# ----- **KWARGS -----
+def print_address(**kwargs):
+    for value in kwargs.values():
+        print(value, end=" ")
+
+print_address(street="123 Fake St.",
+              pobox="P.O Box 777",
+              city="Detroit",
+              state="MI",
+              zip="54321")
+
+# ----- EXERCISE -----
+def shipping_label(*args, **kwargs):
+    for arg in args:
+        print(arg, end=" ")
+    print()
+
+    if "apt" in kwargs:
+        print(f"{kwargs.get('street')} {kwargs.get('apt')}")
+    elif "pobox" in kwargs:
+        print(f"{kwargs.get('street')}")
+        print(f"{kwargs.get('pobox')}")
+    else:
+        print(f"{kwargs.get('street')}")
+
+    print(f"{kwargs.get('city')}, {kwargs.get('state')} {kwargs.get('zip')}")
+
+shipping_label("Dr.", "Spongebob", "Squarepants",
+               street="123 Fake St.",
+               pobox="PO box #1001",
+               city="Detroit",
+               state="MI",
+               zip="54321")
